05ec10f40bb2a9b630565bb86959798eeb9e4157
alex <alex@alex>
2020-12-28 13:16:17 +0100

    hola

--- src/Ejecutable/Main.java
+++ src/Ejecutable/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //
+        //hola
         Usuario[] usuarios = Utilidades.USUARIOS;
         boolean cond = false;
 
